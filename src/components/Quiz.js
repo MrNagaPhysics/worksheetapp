6e458dd8a712a7bdd58af3435790f823ecb9a367
Ragavan <n.ragavan@hotmail.co.uk>
2023-12-03 11:22:26 +0000

    can move between questions

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,5 @@
 import { Box, Button, Typography } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import samplequestions from "./samplequestions.json"
 
@@ -8,44 +8,43 @@ import samplequestions from "./samplequestions.json"
 
 function Quiz() {
 
-  const [questionIndex, setQuestionIndex] = useState(2);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [options,setOptions] = useState([]);
+
+  useEffect(() => {
+    if(samplequestions?.length){
+      const question = samplequestions[questionIndex];
+      setOptions(samplequestions[questionIndex].answers)
+    };
+  }, [samplequestions, questionIndex]);
   
   const {
     question_category,score
   } =useSelector(state=>state);
   console.log(question_category, score)
 
+  const handleClickAnswer = () => {
+    if (questionIndex+1 < samplequestions.length) {
+      setQuestionIndex(questionIndex+1);
+    }
+  
+  }
+
     return (
       <Box>
           <Typography variant="h4" fontWeight="bold">Quiz App</Typography>
           <Typography mt={5} fontSize={18}>{samplequestions[questionIndex].question}</Typography>
 
-          <Box mt={2}>
-            <Button variant="contained" style={{textTransform: 'none'}}>
-              {samplequestions[questionIndex].answers[0]}
-            </Button>
-          </Box>
+          {options.map((data,id) => (
+              <Box mt={2} key={id} >
+              <Button onClick={handleClickAnswer} variant="contained" style={{textTransform: 'none'}}>
+                {data}
+              </Button>
+              </Box>
 
-          <Box mt={2}>
-            <Button variant="contained" style={{textTransform: 'none'}}>
-            {samplequestions[questionIndex].answers[1]}
-            </Button>
-          </Box>
-
-          <Box mt={2}>
-            <Button variant="contained" style={{textTransform: 'none'}}>
-            {samplequestions[questionIndex].answers[2]}
-            </Button>
-          </Box>
-
-          <Box mt={2}>
-            <Button variant="contained" style={{textTransform: 'none'}}>
-            {samplequestions[questionIndex].answers[3]}
-            </Button>
-          </Box>
-
+          ))}
           <Box mt={5}>
-            Score: 2/6
+            Score: {questionIndex}/{samplequestions.length}
           </Box>
       </Box>
     );}
