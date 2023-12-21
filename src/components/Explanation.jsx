4e25106db088c3a92dac95382c13cadfd0bdc6d5
Ragavan <n.ragavan@hotmail.co.uk>
2023-12-21 16:35:01 +0000

    removing last bit of mathJax

--- src/components/Explanation.jsx
+++ src/components/Explanation.jsx
@@ -7,7 +7,7 @@ const Explanation = ({ explanation, isCorrect}) => {
     }
     else if (isCorrect === true) {
         return (
-            explanation ? <Typography fontSize={17}>Correct! Here is the explanation: <MathJax>{explanation}</MathJax></Typography>:<Typography fontSize={17}>No explanation provided</Typography>
+            explanation ? <Typography fontSize={17}>Correct! Here is the explanation:{explanation}</Typography>:<Typography fontSize={17}>No explanation provided</Typography>
         )
     }
     else if (isCorrect === false) {
